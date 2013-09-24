e27c95f7c78f25f60beb5597c0bc6c5a0a9637bf
Csaba Hoch <csaba.hoch@erlang-solutions.com>
2013-09-24 17:21:00 +0200

    Create chat application

---
+++ apps/chat/src/chat_app.erl
@@ -0,0 +1,16 @@
+-module(chat_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    chat_sup:start_link().
+
+stop(_State) ->
+    ok.
