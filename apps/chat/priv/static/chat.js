d2dd0fbd5ca37da9205a4cf9234eada2ae45af3c
Csaba Hoch <csaba.hoch@erlang-solutions.com>
2013-09-25 13:14:07 +0200

    Write web page that interacts with the server

--- apps/chat/priv/static/chat.js
+++ apps/chat/priv/static/chat.js
@@ -1,1 +1,38 @@
-console.log("chat.js loaded");
+var socket;
+
+function add_message(message) {
+    $('#messages').append('<p></p>').children().last().text(message);
+}
+
+function read_message_input() {
+    return $('#message').val();
+}
+
+function connect_to_chat() {
+
+    socket = new WebSocket("ws://localhost:8080/ws");
+
+    socket.onopen = function() {
+        add_message("Connected.")
+    };
+
+    socket.onmessage = function(event) {
+        add_message(event.data);
+    };
+
+    socket.onclose = function() {
+        add_message("Connection closed.");
+    };
+}
+
+function send_message(e) {
+    var message = read_message_input();
+    add_message(message);
+    socket.send(message);
+    $('#message').val("");
+}
+
+$(document).ready(function() {
+    connect_to_chat();
+    $('#send-button').click(send_message);
+})
