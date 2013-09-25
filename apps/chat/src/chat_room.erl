f090d93f926569ef0e17ce2ab30f9280b1496290
Csaba Hoch <csaba.hoch@erlang-solutions.com>
2013-09-25 14:32:43 +0200

    Implement a chat server

--- apps/chat/src/chat_room.erl
+++ apps/chat/src/chat_room.erl
@@ -1,7 +1,7 @@
 -module(chat_room).
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([start_link/0, enter/1, leave/1, send_message/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
@@ -9,7 +9,7 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {}).
+-record(state, {clients=[]}).
 
 %%%=============================================================================
 %%% API
@@ -17,6 +17,15 @@
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+enter(Pid) ->
+    gen_server:cast(?SERVER, {enter, Pid}).
+
+leave(Pid) ->
+    gen_server:cast(?SERVER, {leave, Pid}).
+
+send_message(Pid, Message) ->
+    gen_server:cast(?SERVER, {send_message, Pid, Message}).
 
 %%%=============================================================================
 %%% gen_server callbacks
@@ -49,7 +58,12 @@ init([]) ->
 handle_call(_Request, _From, State) ->
     {noreply, State}.
 
-handle_cast(_Msg, State) ->
+handle_cast({enter, Pid}, State = #state{clients = Clients}) ->
+    {noreply, State#state{clients = [Pid|Clients]}};
+handle_cast({leave, Pid}, State = #state{clients = Clients}) ->
+    {noreply, State#state{clients  = Clients -- [Pid]}};
+handle_cast({send_message, Pid, Message}, State) ->
+    do_send_message(Pid, Message, State),
     {noreply, State}.
 
 handle_info(_Info, State) ->
@@ -64,3 +78,10 @@ code_change(_OldVsn, State, _Extra) ->
 %%%=============================================================================
 %%% Internal functions
 %%%=============================================================================
+
+do_send_message(Pid, Message, #state{clients = Clients}) ->
+    OtherPids = Clients -- [Pid],
+    lists:foreach(
+      fun(OtherPid) ->
+              OtherPid ! {send_message, self(), Message}
+      end, OtherPids).
--- apps/chat/src/chat_ws_handler.erl
+++ apps/chat/src/chat_ws_handler.erl
@@ -11,15 +11,20 @@ init({tcp, http}, _Req, _Opts) ->
     {upgrade, protocol, cowboy_websocket}.
 
 websocket_init(_TransportName, Req, _Opts) ->
+    chat_room:enter(self()),
     {ok, Req, undefined_state}.
 
 websocket_handle({text, Msg}, Req, State) ->
-    {reply, {text, << "You said: ", Msg/binary >>}, Req, State};
+    chat_room:send_message(self(), Msg),
+    {ok, Req, State};
 websocket_handle(_Data, Req, State) ->
     {ok, Req, State}.
 
+websocket_info({send_message, _ServerPid, Msg}, Req, State) ->
+    {reply, {text, Msg}, Req, State};
 websocket_info(_Info, Req, State) ->
     {ok, Req, State}.
 
 websocket_terminate(_Reason, _Req, _State) ->
+    chat_room:leave(self()),
     ok.
